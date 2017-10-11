26d1abd69af523ac0182341574c01ff93d417c8a
ldt_lap <ledangtuanbk@gmail.com>
2017-10-11 17:47:02 +0700

    modify province page

--- src/group/Group.js
+++ src/group/Group.js
@@ -17,11 +17,11 @@ class Group extends Component {
         return (
             <div className="row diseases">
                 <div className="panel panel-default">
-                    <div class="panel-heading">{this.state.groupName}</div>
+                    <div className="panel-heading">{this.state.groupName}</div>
                     <div className="panel-body">
                         {
                             this.state.data.map(({ id, name, numberOfCases }) => (
-                                <div class="row disease-item">
+                                <div className="row disease-item">
                                         <Link to={`/province/${id}`} style={{width: '100%'}}>
                                             <div className="col-md-8 disease-name">{name}</div>
                                             <div className="col-md-4 disease-cases">{numberOfCases}</div>
--- src/province/index.js
+++ src/province/index.js
@@ -6,27 +6,40 @@
  */
 import React, {Component} from 'react';
 import { connect } from "react-redux"
+import "./Province.css"
 
 class ProvincePage extends Component {
     constructor(props) {
         super(props)
-        console.log(props)
         this.state = {
             diseaseId: props.match.params.id
         }
     }
-    render() {
-        return (
-            <p>province page {this.state.diseaseId} </p>
 
+    render() {
+        const { diseases } = this.props;
+        console.log("log",  diseases);
+        return (
+            <div>
+                <div>province name page  </div>
+                <div className="panel-body">
+                    {
+                        diseases.provinces.map(({ name, numberOfCases }) => (
+                            <div className="row disease-item">
+                                <div className="col-md-3 disease-name">{name}</div>
+                                <div className="col-md-4 disease-cases">{numberOfCases}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         )
     }
 }
 
-
 const mapStateToProps = (state, ownProps) => (
     {
-        provinces: state.diseaseStore.diseases[ownProps.match.params.id]
+        diseases: state.diseaseStore.diseases[ownProps.match.params.id],
     }
 )
 
@@ -34,6 +47,5 @@ const mapDispatchToProps = dispatch => {
     return {
     }
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps())(ProvincePage)
