5b56db90bc83cea4e9cf44da526d44d9d44f8a28
polawat phetra <pphetra@gmail.com>
2017-10-11 16:47:28 +0700

    connect province page with redux store.

--- src/province/index.js
+++ src/province/index.js
@@ -5,10 +5,12 @@
  * Created by pphetra on 11/10/2017 AD.
  */
 import React, {Component} from 'react';
+import { connect } from "react-redux"
 
 class ProvincePage extends Component {
     constructor(props) {
         super(props)
+        console.log(props)
         this.state = {
             diseaseId: props.match.params.id
         }
@@ -16,8 +18,22 @@ class ProvincePage extends Component {
     render() {
         return (
             <p>province page {this.state.diseaseId} </p>
+
         )
     }
 }
 
-export default ProvincePage
+
+const mapStateToProps = (state, ownProps) => (
+    {
+        provinces: state.diseaseStore.diseases[ownProps.match.params.id]
+    }
+)
+
+const mapDispatchToProps = dispatch => {
+    return {
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps())(ProvincePage)
